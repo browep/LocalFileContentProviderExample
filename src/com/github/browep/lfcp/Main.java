9be96b80f8f4c1a613f71033ecaccfeade06938b
paul <paul@paul-Pangolin-Performance.(none)>
2012-02-23 20:51:41 -0700

    readme addition

--- src/com/github/browep/lfcp/Main.java
+++ src/com/github/browep/lfcp/Main.java
@@ -1,14 +1,10 @@
 package com.github.browep.lfcp;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 import java.io.*;
 
